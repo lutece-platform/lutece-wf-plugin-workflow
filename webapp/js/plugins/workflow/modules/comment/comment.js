46fe0fb93b40144edcacf56c2f0fea2603d09100
Cyril Moire <cyril.moire@atos.net>
2016-06-17 17:10:36 +0200

    WORKFLOW-109 : dynamically create form because IE does not support changing window.location

--- webapp/js/plugins/workflow/modules/comment/comment.js
+++ webapp/js/plugins/workflow/modules/comment/comment.js
@@ -1,3 +1,8 @@
 function removeComment(idHistory, idTask) {
-  window.location = "jsp/admin/plugins/workflow/modules/comment/ConfirmRemoveComment.jsp?" + "id_history=" + idHistory + "&id_task=" + idTask + "&return_url=" + encodeURIComponent(window.location.href);
+  var form = $('<form>', 
+    { 'action': 'jsp/admin/plugins/workflow/modules/comment/ConfirmRemoveComment.jsp?' + 'id_history=' + idHistory + '&id_task=' + idTask + '&return_url=' + encodeURIComponent(window.location.href),
+      'method':'post'}
+    );
+    form.appendTo('body');
+    form.submit().remove();
 }
