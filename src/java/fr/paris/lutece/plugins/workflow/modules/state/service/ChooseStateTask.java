3ef4d75d307131d645ca2baf65d2d657fb4b91a1
Guillaume Lépine <guillaume.lepine@sogeti.com>
2021-04-23 10:43:50 +0200

    WORKFLOW-172: Implements doRemoveConfig

--- src/java/fr/paris/lutece/plugins/workflow/modules/state/service/ChooseStateTask.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/state/service/ChooseStateTask.java
@@ -2,9 +2,12 @@ package fr.paris.lutece.plugins.workflow.modules.state.service;
 
 import java.util.Locale;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 
 import fr.paris.lutece.plugins.workflow.modules.state.business.ChooseStateTaskInformationHome;
+import fr.paris.lutece.plugins.workflowcore.service.config.ITaskConfigService;
 import fr.paris.lutece.plugins.workflowcore.service.task.SimpleTask;
 import fr.paris.lutece.portal.service.i18n.I18nService;
 
@@ -12,6 +15,10 @@ public class ChooseStateTask extends SimpleTask
 {
     private static final String MESSAGE_MARK_TITLE = "module.workflow.state.task.choose.state.title";
 
+    @Inject
+    @Named( "workflow.chooseStateTaskConfigService" )
+    private ITaskConfigService _chooseStateTaskConfigService;
+    
     @Override
     public void processTask( int nIdResourceHistory, HttpServletRequest request, Locale locale )
     {
@@ -28,5 +35,11 @@ public class ChooseStateTask extends SimpleTask
     public void doRemoveTaskInformation( int nIdHistory )
     {
         ChooseStateTaskInformationHome.remove( nIdHistory, getId( ) );
+    }
+    
+    @Override
+    public void doRemoveConfig( )
+    {
+        _chooseStateTaskConfigService.remove( getId( ) );
     }
 }
