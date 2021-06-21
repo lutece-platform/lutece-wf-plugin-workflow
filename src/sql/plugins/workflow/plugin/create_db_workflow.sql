0a30ba7d9e1ad2856dbae8480078a71bc349bf43
Guillaume Lépine <guillaume.lepine@sogeti.com>
2021-06-21 11:56:31 +0200

    WORKFLOW-173: Fixes script

--- src/sql/plugins/workflow/plugin/create_db_workflow.sql
+++ src/sql/plugins/workflow/plugin/create_db_workflow.sql
@@ -312,8 +312,7 @@ CREATE TABLE workflow_task_choose_state_information (
 DROP TABLE IF EXISTS workflow_task_change_state_config;
 CREATE TABLE workflow_task_change_state_config (
 	 id_task INT DEFAULT 0 NOT NULL ,
-	 id_next_state INT NOT NULL,
-	 id_state_ko INT NOT NULL,
+	 id_next_state INT NOT NULL
 );
 
 DROP TABLE IF EXISTS workflow_task_change_state_information;
--- src/sql/plugins/workflow/upgrade/update_db_workflow-5.1.0-5.2.0.sql
+++ src/sql/plugins/workflow/upgrade/update_db_workflow-5.1.0-5.2.0.sql
@@ -1,8 +1,7 @@
 DROP TABLE IF EXISTS workflow_task_change_state_config;
 CREATE TABLE workflow_task_change_state_config (
 	 id_task INT DEFAULT 0 NOT NULL ,
-	 id_next_state INT NOT NULL,
-	 id_state_ko INT NOT NULL,
+	 id_next_state INT NOT NULL
 );
 
 DROP TABLE IF EXISTS workflow_task_change_state_information;
