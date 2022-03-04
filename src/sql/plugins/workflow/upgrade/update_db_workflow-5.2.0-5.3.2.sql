bedd7788fa260a25301e87b7945267012d98de34
omar.ben.haddi <omar.ben.haddi@CPX-G3EEG1B1URZ.dir.svc.accenture.com>
2022-03-04 11:09:50 +0100

    WORKFLOW-200 : Add a new workflow task Confirm Action

---
+++ src/sql/plugins/workflow/upgrade/update_db_workflow-5.2.0-5.3.2.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS workflow_task_confirm_action_config;
+CREATE TABLE workflow_task_confirm_action_config (
+	 id_task INT DEFAULT 0 NOT NULL ,
+	 message LONG VARCHAR NOT NULL
+);
