7231458df378565bf3045785a3d1d88258dfc0ca
sebastien leridon <sebleridon@laposte.net>
2022-12-08 19:17:22 +0100

    WORKFLOW-200 : Add init script for workflow task Confirm Action

--- src/sql/plugins/workflow/plugin/create_db_workflow.sql
+++ src/sql/plugins/workflow/plugin/create_db_workflow.sql
@@ -321,6 +321,14 @@ CREATE TABLE workflow_task_change_state_information (
   new_state VARCHAR(255) NOT NULL
 );
 
+-- ---------------------------------------------------------------------
+-- Table structure for table workflow_task_confirm_action_config 	  --
+-- ---------------------------------------------------------------------
+CREATE TABLE workflow_task_confirm_action_config (
+	 id_task INT DEFAULT 0 NOT NULL ,
+	 message LONG VARCHAR NOT NULL
+);
+
 CREATE TABLE workflow_action_state_before (
 id_action int NOT NULL,
 id_state_before int NOT NULL,
