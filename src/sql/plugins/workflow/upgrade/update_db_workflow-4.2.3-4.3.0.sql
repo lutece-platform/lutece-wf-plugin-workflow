f4bef32d01b65d2bef91045f3e947ea6f75e7ff1
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-07-25 16:01:19 +0200

    LUT-30456 : Prepare SQL scripts for Liquibase plugin

--- src/sql/plugins/workflow/upgrade/update_db_workflow-4.2.3-4.3.0.sql
+++ src/sql/plugins/workflow/upgrade/update_db_workflow-4.2.3-4.3.0.sql
@@ -1,1 +1,4 @@
+--liquibase formatted sql
+--changeset workflow:update_db_workflow-4.2.3-4.3.0.sql
+--preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE workflow_task_comment_config ADD COLUMN	is_richtext SMALLINT DEFAULT 0;
--- src/sql/plugins/workflow/upgrade/update_db_workflow-4.3.6-4.3.7.sql
+++ src/sql/plugins/workflow/upgrade/update_db_workflow-4.3.6-4.3.7.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+--changeset workflow:update_db_workflow-4.3.6-4.3.7.sql
+--preconditions onFail:MARK_RAN onError:WARN
 /*
     Note : AUTO_INCREMENT fields work with Mysql. For PostgreSQL, you have to 
     replace AUTO_INCREMENT, depending on your PostgreSQL version. You can
