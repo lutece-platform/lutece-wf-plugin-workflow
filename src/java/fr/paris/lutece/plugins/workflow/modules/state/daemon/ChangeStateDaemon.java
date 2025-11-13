673c9e15254f709c11172c60473b35b7836592b2
Jeremy Deloche <jeremy.deloche@atos.com>
2025-11-13 15:20:40 +0100

    LUT-31041 : Stop the daemon from sending notifications for inactive workflows

--- src/java/fr/paris/lutece/plugins/workflow/modules/state/daemon/ChangeStateDaemon.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/state/daemon/ChangeStateDaemon.java
@@ -70,7 +70,7 @@ public class ChangeStateDaemon extends Daemon
     public void run( )
     {
         WorkflowFilter workflowFilter = new WorkflowFilter( );
-        workflowFilter.setIsEnabled( 1 );
+        workflowFilter.setIsEnabled( WorkflowFilter.FILTER_TRUE );
 
         List<Workflow> listWorkflows = _workflowService.getListWorkflowsByFilter( workflowFilter );
 
